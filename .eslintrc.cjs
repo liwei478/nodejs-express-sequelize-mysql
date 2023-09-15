22a8cedf4e45fae539cb98ccc18aeae9698f2e8b
lwz <lwz@example.com>
2023-09-15 20:50:01 +0800

    feat: use antfu eslint-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,8 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  extends: "@antfu",
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'package.json'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
